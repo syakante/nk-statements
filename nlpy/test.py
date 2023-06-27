8ef12e36b8a15fa95c67034e538e2238849d581c
Shelley Kim <SKim@csis.org>
2023-06-27 15:00:50 -0400

    minor changes

--- nlpy/test.py
+++ nlpy/test.py
@@ -1,4 +1,21 @@
 from kiwipiepy import Kiwi
+from kiwipiepy.utils import Stopwords
 
-k = Kiwi()
-s = "조미관계 교착은 조선반도비핵화에 대한 미국의 그릇된 인식"
+#k = Kiwi()
+#s = "스웨리예공산당 핵 위협 장본인은 미국 인터네트에 게재"
+
+from htmldate import find_date
+
+def getDate(url) -> str:
+    # if(pd.isnull(url)):
+    #     return(None)
+    try:
+        print("trying", url)
+        ret = find_date(url)
+    except:
+        print("error!")
+        return(None)
+    return(ret)
+
+myurl = "https://www.straitstimes.com/asia/east-asia/united-airlines-avoids-listing-taiwan-hk-as-part-of-china-by-using-flexible"
+getDate(myurl)
